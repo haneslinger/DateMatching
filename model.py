01ee1ae6e1bca5ea81f3830eebc9bccb8cefa237
unknown <haneslin@cs.du.edu>
2018-08-23 11:29:28 -0600

    pretty LR

--- model.py
+++ model.py
@@ -1,34 +1,43 @@
 import numpy as np
-from keras.models import Sequential
-from keras.layers import Dense
+#from keras.models import Sequential
+#from keras.layers import Dense
 
-def trainModel(x, y):
-    # build model
-    model = Sequential()    
-    model.add(Dense(input_dim  = 1,
+class My_Model():
+    def __init__(self):
+        # init member varible model
+        self.model = Sequential() 
+        # build model
+        self.model.add(Dense(input_dim  = 1,
                     output_dim = 1,
                     init = 'normal',
                     activation = 'linear',
                     use_bias = True))
+        # compile model
+        self.model.compile(loss='mean_squared_error',
+                      optimizer='SGD',
+                      metrics=['accuracy'])
 
-    # compile model
-    model.compile(loss='mean_squared_error',
-                  optimizer='SGD',
-                  metrics=['accuracy'])
-    
-    # train model
-    model.fit(x, y, epochs = 200, verbose = 1)
+    def trainModel(self, x, y):    
+        # train model
+        self.model.fit(x, y, epochs = 200, verbose = 1)
 
-    # test model
-    test =  np.array([0, .2, -3, 2, -5])
-    print(model.predict(test, verbose = 1))
+    def testModel(self, x):
+        # test model
+        return (self.model.predict(x, verbose = 1))
 
 # main
 if __name__ == '__main__':
+    # create data
     x = np.linspace(-1,1,101)
     y = x*2
     
-    trainModel(x, y)
+    # create and train model
+    model = My_Model()
+    model.trainModel(x, y)
+    
+    # test model
+    test = np.array([0, .2, -3, 2, -5]) 
+    model.testModel(x)
     
     print("I did it")
     
