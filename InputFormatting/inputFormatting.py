6c050ddcb28d66d3d2569285f0a979992cbbc9bc
unknown <haneslin@cs.du.edu>
2018-08-27 12:58:37 -0600

    formatting input(1)

---
+++ InputFormatting/inputFormatting.py
@@ -0,0 +1,9 @@
+def pharseToOneHot( pharse):
+    for char in pharse:
+        print(ord(char))
+
+
+if __name__ == '__main__':
+    print('we are going to do some formatting')
+    pharseToOneHot("Hannah")
+    pass
